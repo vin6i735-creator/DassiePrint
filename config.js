177d3f7b932c788eb6024906a89a060f53b91c72
vin6i735-creator <vin6i735@gmail.com>
2026-06-23 08:50:17 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+const CONFIG = {
+
+empresa: "DassiePrint",
+
+versao: "2.0",
+
+moeda: "BRL",
+
+tema: "escuro",
+
+senhaAdmin: "123456",
+
+statusPedido:[
+"Recebido",
+"Produção",
+"Pronto",
+"Entregue"
+],
+
+statusPagamento:[
+"Pendente",
+"Parcial",
+"Pago"
+]
+
+}
